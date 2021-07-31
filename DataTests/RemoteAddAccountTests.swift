9088ba6502627cde25567b98e93b28d13f91362c
Alexandre Robaert <robaert.developer@icloud.com>
2021-07-31 20:03:45 -0300

    Test de HttpPostClient com mesma URL

---
+++ DataTests/RemoteAddAccountTests.swift
@@ -0,0 +1,46 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by Alexandre Robaert on 31/07/21.
+//
+
+import XCTest
+
+class RemoteAddAccount {
+    private let url: URL
+    private let httpClient: HttpPostClient
+    
+    init(url: URL, httpClient: HttpPostClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    func add(){
+        httpClient.post(url: url)
+    }
+}
+
+protocol HttpPostClient {
+    func post(url: URL)
+}
+
+class RemoteAddAccountTests: XCTestCase {
+    
+    func test_() throws {
+        let url = URL(string: "http://any-url.com")!
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        sut.add()
+        XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    class HttpClientSpy: HttpPostClient {
+        
+        var url: URL?
+        func post(url: URL) {
+            self.url = url
+        }
+    }
+
+}
