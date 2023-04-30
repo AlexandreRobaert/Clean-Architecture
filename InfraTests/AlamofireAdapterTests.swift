50de8ab385ed9e03687fb73f44038a5e28e72e5d
Alexandre Robaert <Robaert.developer@icloud.com>
2023-04-30 19:13:48 -0300

    Criado a função makeSut

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -16,8 +16,9 @@ class AlamofireAdapter {
         self.session = session
     }
     
-    func post(url: URL) {
-        session.request(url, method: .post).resume()
+    func post(url: URL, with data: Data?) {
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .fragmentsAllowed) as? [String: Any]
+        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
@@ -25,20 +26,44 @@ final class AlamofireAdapterTests: XCTestCase {
 
     func test_post_request_alamofire_url_e_method_corretos() throws {
         let url = URL(string: "https://any-url.com")!
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [URLProtocolStub.self]
-        let session = Session(configuration: configuration)
-        let sut = AlamofireAdapter(session: session)
+        let sut = makeSut()
         let expectation = expectation(description: "Fazendo o Post")
-        sut.post(url: url)
+        
+        let data = #"{"name": "Alexandre"}"#.data(using: .utf8)
+        
+        sut.post(url: url, with: data)
         URLProtocolStub.observerCompletion { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual(HTTPMethod.post.rawValue, request.method?.rawValue)
+            XCTAssertNotNil(request.httpBodyStream)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func test_post_request_alamofire_sem_dados() throws {
+    
+        let sut = makeSut()
+        let expectation = expectation(description: "Fazendo o Post")
+        sut.post(url: URL(string: "https://any-url.com")!, with: nil)
+        URLProtocolStub.observerCompletion { request in
+            XCTAssertNil(request.httpBodyStream)
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 1)
     }
 }
+
+private extension AlamofireAdapterTests {
+    
+    func makeSut() -> AlamofireAdapter {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = Session(configuration: configuration)
+        return AlamofireAdapter(session: session)
+    }
+}
+
 
 class URLProtocolStub: URLProtocol {
     
