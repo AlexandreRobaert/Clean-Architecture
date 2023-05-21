ef44ada04be62b68e9107da1114db03772d0763f
Alexandre Robaert <Robaert.developer@icloud.com>
2023-05-20 21:41:36 -0300

    criado método validate para o SignupViewModel

--- Presentation/Presenters/SignupPresenter.swift
+++ Presentation/Presenters/SignupPresenter.swift
@@ -18,34 +18,40 @@ public class SignupPresenter {
     
     public func signUp(viewModel: SignupViewModel) {
         
-        guard let name = viewModel.name, !name.isEmpty else {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Nome é obrigatório"))
-            return
+        let result = validate(signupViewModel: viewModel)
+        if result.isValid {
+            // Adiciona Usuário
+        } else if let alertViewModel = result.alertViewModel {
+            alertView.showMessage(viewModel: alertViewModel)
+        }
+    }
+    
+    private func validate(signupViewModel: SignupViewModel) -> (isValid: Bool, alertViewModel: AlertViewModel?) {
+        guard let name = signupViewModel.name, !name.isEmpty else {
+            return (false, AlertViewModel(title: "Falha na validação", message: "Nome é obrigatório"))
         }
         
-        guard let email = viewModel.email, !email.isEmpty else {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Email é obrigatório"))
-            return
+        guard let email = signupViewModel.email, !email.isEmpty else {
+            return (false, AlertViewModel(title: "Falha na validação", message: "Email é obrigatório"))
         }
         
-        guard let password = viewModel.password, !password.isEmpty,
-              let passwordConfirmation = viewModel.passwordConfirmation,
+        guard let password = signupViewModel.password, !password.isEmpty,
+              let passwordConfirmation = signupViewModel.passwordConfirmation,
               !passwordConfirmation.isEmpty else {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Senha/Confirmar Senha é obrigatória"))
-            return
+            return (false, AlertViewModel(title: "Falha na validação", message: "Senha/Confirmar Senha é obrigatória"))
         }
         
-        if let password = viewModel.password,
-           let passwordConfirmation = viewModel.passwordConfirmation,
+        if let password = signupViewModel.password,
+           let passwordConfirmation = signupViewModel.passwordConfirmation,
            !password.elementsEqual(passwordConfirmation) {
             
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "Senhas não conferem"))
-            return
+            return (false, AlertViewModel(title: "Falha na validação", message: "Senhas não conferem"))
         }
         
         if !emailValidator.isValid(email: email) {
-            alertView.showMessage(viewModel: .init(title: "Falha na validação", message: "Email inválido"))
+            return (false, AlertViewModel(title: "Falha na validação", message: "Email inválido"))
         }
+        return (true, nil)
     }
 }
 
