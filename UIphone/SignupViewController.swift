4c06bd03576d842bff0afcd3f12c9f531c3c4e42
Alexandre Robaert <robaert.developer@icloud.com>
2023-05-28 20:54:54 -0300

    Adicionado teste do botão salvar na SignupViewController

--- UIphone/SignupViewController.swift
+++ UIphone/SignupViewController.swift
@@ -6,21 +6,35 @@
 //
 
 import UIKit
+import Domain
 import Presentation
 
 public final class SignupViewController: UIViewController {
     
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet var fields: [UITextField]!
     
     public var isLoading: Bool = false {
         didSet {
             toogleLoading()
         }
     }
+    public var signupAction: ((_ viewModel: AddAccountModel) -> Void)?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configure()
+    }
+    
+    private func configure() {
+        saveButton.addTarget(self, action: #selector(saveButtonTap), for: .touchUpInside)
+    }
+    
+    @objc
+    private func saveButtonTap() {
+        let addAccountModel: AddAccountModel = .init(name: fields[0].text!, email: fields[1].text!, password: fields[2].text!, passwordConfirmation: fields[3].text!)
+        signupAction?(addAccountModel)
     }
 }
 
--- UIphoneTests/SignupViewControllerTest.swift
+++ UIphoneTests/SignupViewControllerTest.swift
@@ -8,6 +8,7 @@
 import XCTest
 import UIKit
 import Presentation
+import Domain
 @testable import UIphone
 
 final class SignupViewControllerTest: XCTestCase {
@@ -26,13 +27,40 @@ final class SignupViewControllerTest: XCTestCase {
         let sut = makeSut()
         XCTAssertEqual(sut is AlertViewProtocol, true)
     }
+    
+    func test_deve_chamar_signup_quando_clicar_no_botao_com_dados_corretos() throws {
+        var addAccoutModel: AddAccountModel?
+        let sut = makeSut { addAccoutModel = $0 }
+        sut.saveButton.simulateTap()
+        let name = sut.fields[0].text!
+        let email = sut.fields[1].text!
+        let password = sut.fields[2].text!
+        let passwordConfirmation = sut.fields[3].text!
+        XCTAssertEqual(addAccoutModel, .init(name: name, email: email, password: password,
+                                             passwordConfirmation: passwordConfirmation))
+    }
 }
 
 extension SignupViewControllerTest {
-    func makeSut() -> SignupViewController {
+    func makeSut(signupActionSpy: ((AddAccountModel) -> Void)? = nil) -> SignupViewController {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignupViewController.self))
         let sut = sb.instantiateViewController(withIdentifier: "SignupViewController") as! SignupViewController
+        sut.signupAction = signupActionSpy
         sut.loadViewIfNeeded()
         return sut
+    }
+}
+
+private extension UIControl {
+    private func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach({ action in
+                (target as NSObject).perform(Selector(action))
+            })
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
     }
 }
